e0d9b7937cd5bbfe55db8ceb3edd0b0facbaa828
hardikaureate <hardik.devani@aureatelabs.com>
2021-11-30 14:48:36 +0530

    1 file modified

--- src/components/Navigation.client.jsx
+++ src/components/Navigation.client.jsx
@@ -1,10 +1,12 @@
 import {Link} from '@shopify/hydrogen/client';
 
 export default function Navigation({collections}) {
+  console.log('hdrcategory',collections)
   return (
     <nav className="hidden lg:block text-center">
       <ul className="md:flex items-center justify-center">
-        {collections.map((collection) => console.log('headercollection',collection)(
+        {collections.map((collection) => 
+        console.log('headercollection',collection)(
           <li key={collection.id}>
             <Link
               to={`/collections/${collection.handle}`}
