a48240ae033ef22fdd89cb041dcc2a952aa3e1c3
hardikaureate <hardik.devani@aureatelabs.com>
2021-11-30 12:31:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# shopify-hydrogen-xskk43
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/shopify-hydrogen-xskk43)
---
+++ shopify.config.js
@@ -0,0 +1,6 @@
+export default {
+  locale: 'en-us',
+  storeDomain: 'al-rnd.myshopify.com',
+  storefrontToken: 'fb15d7114123e18fbc9c559328b61c86',
+  graphqlApiVersion: 'unstable',
+};
