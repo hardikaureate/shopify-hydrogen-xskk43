34a35d48b37866e24b9335dc9ee3a75c738b1b74
hardikaureate <hardik.devani@aureatelabs.com>
2021-11-30 15:01:46 +0530

    2 files modified

--- src/components/Footer.server.jsx
+++ src/components/Footer.server.jsx
@@ -81,12 +81,6 @@ export default function Footer({ collection, product }) {
           <div>
             <h2 className="text-md font-medium uppercase mb-4">Templates</h2>
             <ul className="mt-8 space-y-4">
-              {/* <li className="flex items-center text-sm font-medium text-gray-600 hover:text-gray-900">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="flex items-center text-sm font-medium text-gray-600 hover:text-gray-900">
-                <Link to={`/products/${product?.handle}`}>Product</Link>
-              </li> */}
               {allcollections && allcollections.map((footercollection, i) => {
                 return (
                   <li>
@@ -96,11 +90,6 @@ export default function Footer({ collection, product }) {
                   </li>
                 )
               })}
-              {/* <li className="flex items-center text-sm font-medium text-gray-600 hover:text-gray-900">
-                <Link to={`/collections/${collection?.handle}`}>
-                  Collection
-                </Link>
-              </li> */}
             </ul>
           </div>
           {/* eslint-disable-next-line @shopify/jsx-prefer-fragment-wrappers */}
--- src/components/Navigation.client.jsx
+++ src/components/Navigation.client.jsx
@@ -1,10 +1,28 @@
 import {Link} from '@shopify/hydrogen/client';
+import {
+  useShopQuery,
+  flattenConnection,
+} from '@shopify/hydrogen';
+import gql from 'graphql-tag';
 
 export default function Navigation({collections}) {
   console.log('hdrcategory',collections)
+  const { data } = useShopQuery({
+    query: QUERY,
+    variables: {
+      numCollections: 5,
+    },
+    cache: {
+      maxAge: 60,
+      staleWhileRevalidate: 60 * 10,
+    },
+  });
+
+  const allcollections = data ? flattenConnection(data.collections) : null;
   return (
     <nav className="hidden lg:block text-center">
       <ul className="md:flex items-center justify-center">
+        {console.log('allcollections',allcollections)}
         {collections.map((collection) => (
           <li key={collection.id}>
             <Link
@@ -19,3 +37,33 @@ export default function Navigation({collections}) {
     </nav>
   );
 }
+
+
+const QUERY = gql`
+  query indexContent($numCollections: Int!) {
+    shop {
+      name
+    }
+    collections(first: $numCollections) {
+      edges {
+        node {
+          description
+          handle
+          id
+          title
+          image {
+            ...ImageFragment
+          }
+        }
+      }
+    }
+    products(first: 1) {
+      edges {
+        node {
+          handle
+        }
+      }
+    }
+  }
+  ${Image.Fragment}
+`;
