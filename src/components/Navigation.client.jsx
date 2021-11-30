daf58db905596df6d25b9e40bb6495e4477a3698
hardikaureate <hardik.devani@aureatelabs.com>
2021-11-30 15:12:20 +0530

    1 file modified

--- src/components/Navigation.client.jsx
+++ src/components/Navigation.client.jsx
@@ -1,28 +1,9 @@
-import {Link} from '@shopify/hydrogen/client';
-import {
-  useShopQuery,
-  flattenConnection,
-} from '@shopify/hydrogen';
-import gql from 'graphql-tag';
+import { Link } from '@shopify/hydrogen/client';
 
-export default function Navigation({collections}) {
-  //console.log('hdrcategory',collections)
-  const { data } = useShopQuery({
-    query: QUERY,
-    variables: {
-      numCollections: 3,
-    },
-    cache: {
-      maxAge: 60,
-      staleWhileRevalidate: 60 * 10,
-    },
-  });
-
-  //const allcollections = data ? flattenConnection(data.collections) : null;
+export default function Navigation({ collections }) {
   return (
     <nav className="hidden lg:block text-center">
       <ul className="md:flex items-center justify-center">
-        {/* {console.log('allcollections',allcollections)} */}
         {collections.map((collection) => (
           <li key={collection.id}>
             <Link
@@ -37,29 +18,3 @@ export default function Navigation({collections}) {
     </nav>
   );
 }
-
-
-const QUERY = gql`
-  query indexContent($numCollections: Int!) {
-    shop {
-      name
-    }
-    collections(first: $numCollections) {
-      edges {
-        node {
-          description
-          handle
-          id
-          title
-        }
-      }
-    }
-    products(first: 1) {
-      edges {
-        node {
-          handle
-        }
-      }
-    }
-  }
-`;
