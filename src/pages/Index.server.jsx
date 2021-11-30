76ccefe64e4e690b6e5098840b0afd2360103851
hardikaureate <hardik.devani@aureatelabs.com>
2021-11-30 17:23:35 +0530

    1 file modified

--- src/pages/Index.server.jsx
+++ src/pages/Index.server.jsx
@@ -103,7 +103,7 @@ export default function Index({country = {isoCode: 'US'}}) {
                   </Link>
                 </span>
               </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 mb-8">
                 {featuredProducts.map((product) => (
                   <div key={product.id}>
                     <ProductCard product={product} />
