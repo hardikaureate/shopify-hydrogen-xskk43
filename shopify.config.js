671d6af1cb4e11e35630f824fd3f639635e83a90
hardikaureate <hardik.devani@aureatelabs.com>
2021-11-30 15:50:42 +0530

    1 file modified

--- shopify.config.js
+++ shopify.config.js
@@ -1,6 +1,6 @@
 export default {
   locale: 'en-us',
-  storeDomain: 'al-rnd.myshopify.com',
-  storefrontToken: 'fb15d7114123e18fbc9c559328b61c86',
+  storeDomain: 'sacheustore.myshopify.com',
+  storefrontToken: '251aff87463a43306b6c981179644ecf',
   graphqlApiVersion: 'unstable',
 };
