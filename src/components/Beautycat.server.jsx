77d00dca0a9a46113c8b39891d466a6cca66389d
hardikaureate <hardik.devani@aureatelabs.com>
2021-12-02 14:22:49 +0530

    1 file modified

--- src/components/Beautycat.server.jsx
+++ src/components/Beautycat.server.jsx
@@ -1,3 +1,7 @@
+
+
+
+
 import {
   useShopQuery,
   flattenConnection,
@@ -6,110 +10,117 @@ import {
   Link,
 } from '@shopify/hydrogen';
 import gql from 'graphql-tag';
-
-
+import NotFound from './NotFound.server';
+//import FeaturedCollection from './FeaturedCollection.server';
 import ProductCard from './ProductCard.server';
+//import Layout from './Layout.server';
 
-
-export default function Beautycat({country = {isoCode: 'US'}}) {
-  const {data} = useShopQuery({
-    query: QUERY
-    
+export default function Beautycat({ country = { isoCode: 'US' } }) {
+  const { data } = useShopQuery({
+    query: QUERY,
+    variables: {
+      country: country.isoCode,
+      //numCollections: 5,
+    },
   });
+
 
   const collections = data ? flattenConnection(data.collections) : [];
   const featuredProductsCollection = collections[2];
   const featuredProducts = featuredProductsCollection
     ? flattenConnection(featuredProductsCollection.products)
     : null;
-  const featuredCollection =
-    collections && collections.length > 1 ? collections[1] : collections[0];
-  console.log('beautyProduct', collections )
+  //const featuredCollection = collections && collections.length > 1 ? collections[1] : collections[0];
+  //console.log({ collections })
 
   return (
-   
-      <div className="relative mb-12">
-        
-        <div className="bg-white p-12 shadow-xl rounded-xl mb-10">
-          {featuredProductsCollection ? (
-            <>
+
+    <>
+      {featuredProductsCollection ? (
+        <>
+          <div className="FeaturedProducts">
+            <div className="container">
               <div className="flex justify-between items-center mb-8 text-md font-medium">
                 <span className="text-black uppercase">
-                  {featuredProductsCollection.title}
+                  <h2>{featuredProductsCollection.title}</h2>
                 </span>
-                <span className="hidden md:inline-flex">
-                  <Link
-                    to={`/collections/${featuredProductsCollection.handle}`}
-                    className="text-blue-600 hover:underline"
-                  >
-                    Shop all
-                  </Link>
-                </span>
+                {/* <span className="hidden md:inline-flex">
+                    <Link
+                      to={`/collections/${featuredProductsCollection.handle}`}
+                      className="text-blue-600 hover:underline"
+                    >
+                      Shop all
+                    </Link>
+                  </span> */}
               </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
-                {featuredProducts.map((product) => (
-                  <div key={product.id}>
-                    <ProductCard product={product} />
+              <div className="container">
+                <div className="productContainer">
+                  <div className="product-listing">
+                  
+                    {featuredProducts.map((product) => (
+
+                      <div key={product.id} className="itemBox">
+                        <div className="item">
+                          <ProductCard product={product} />
+                        </div>
+                      </div>
+                    ))}
+                   
                   </div>
-                ))}
+                </div>
               </div>
-              <div className="md:hidden text-center">
+              {/* <div className="md:hidden text-center">
                 <Link
                   to={`/collections/${featuredCollection.handle}`}
                   className="text-blue-600"
                 >
                   Shop all
                 </Link>
-              </div>
-            </>
-          ) : null}
-        </div>
-        
-      </div>
-    
+              </div> */}
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
   );
 }
 
 const QUERY = gql`
-query indexContent(
-  $country: CountryCode
-  # $numCollections: Int = 2
-  # $numProducts: Int = 3
-  $numProductMetafields: Int = 0
-  $numProductVariants: Int = 20
-  $numProductMedia: Int = 1
-  $numProductVariantMetafields: Int = 10
-  $numProductVariantSellingPlanAllocations: Int = 0
-  $numProductSellingPlanGroups: Int = 0
-  $numProductSellingPlans: Int = 0
-) @inContext(country: $country) {
-  collections(first: 3) {
-    edges {
-      node {
-        descriptionHtml
-        description
-        handle
-        id
-        title
-        image {
-          ...ImageFragment
-        }
-        products(first: 7) {
-          edges {
-            node {
-              ...ProductProviderFragment
+  query indexContent(
+    $country: CountryCode
+    # $numCollections: Int = 2
+    # $numProducts: Int = 3
+    $numProductMetafields: Int = 0
+    $numProductVariants: Int = 20
+    $numProductMedia: Int = 1
+    $numProductVariantMetafields: Int = 10
+    $numProductVariantSellingPlanAllocations: Int = 0
+    $numProductSellingPlanGroups: Int = 0
+    $numProductSellingPlans: Int = 0
+  ) @inContext(country: $country) {
+    collections(first: 3) {
+      edges {
+        node {
+          descriptionHtml
+          description
+          handle
+          id
+          title
+          image {
+            ...ImageFragment
+          }
+          products(first: 7) {
+            edges {
+              node {
+                ...ProductProviderFragment
+              }
             }
           }
         }
       }
     }
   }
-}
 
-${ProductProviderFragment}
-${Image.Fragment}
+  ${ProductProviderFragment}
+  ${Image.Fragment}
 `;
-
-
-
-
